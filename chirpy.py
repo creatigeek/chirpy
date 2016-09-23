b8e945da24092bf57a0f39687e449e722141e1a3
Vanessa <vwallis@gmail.com>
2016-09-22 18:33:33 -0700

    adds chirpy.py and initial chirpy print

---
+++ chirpy.py
@@ -0,0 +1,21 @@
+import sys
+import re
+
+""" chirpy welcomes you to the land of regular expressions. (:
+	It will happily receive the well-formed and assumed file with 
+	formatting agreed upon offline. 
+
+"""
+
+def print_chirpy():
+	""" original source: http://around140.en.utf8art.com/arc/pig_102.html
+		modified for command line fit and fun ;p
+	"""
+	print("　　~ﾍ⌒ヽﾌ~\t\tHi there, I'm chirpy.\n\
+.  ( ・ω・）\t\tLet's parse a file\n\
+　 ﾉ\")   )\t\t    and...\n\
+  彡ノ,,ノ\t\tprint a report! Yay!\n\
+---―〃-〃――――\n\
+　　ﾚ,,/\n")
+
+print_chirpy()
