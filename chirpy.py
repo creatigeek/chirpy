5b952099f7529b013a9be8a9bbc0775b84226eec
Vanessa <vwallis@gmail.com>
2016-09-26 16:26:40 -0700

    aligns chirpy character, adds parsing function and regex pattern that works for all lines in input file

--- chirpy.py
+++ chirpy.py
@@ -11,11 +11,39 @@ def print_chirpy():
 	""" original source: http://around140.en.utf8art.com/arc/pig_102.html
 		modified for command line fit and fun ;p
 	"""
-	print("　　~ﾍ⌒ヽﾌ~\t\tHi there, I'm chirpy.\n\
+	print("\n\
+　　~ﾍ⌒ヽﾌ~\t\tHi there, I'm chirpy.\n\
 .  ( ・ω・）\t\tLet's parse a file\n\
 　 ﾉ\")   )\t\t    and...\n\
   彡ノ,,ノ\t\tprint a report! Yay!\n\
 ---―〃-〃――――\n\
-　　ﾚ,,/\n")
+　　ﾚ,,/\n\n")
+
+def parse_file(input_obj):
+	count_lines = 0
+	tn_count = 0
+
+	tn_pattern = re.compile(r'\.(?P<tn_size>\d{2,3}x\d{2,3})_q85_crop_upscale\.\w+')
+	
+	for line in input_obj:
+		# print(line)
+		count_lines += 1
+		results = re.search(tn_pattern, line)
+		if results is not None:
+			# print(results.group('tn_size'))
+			tn_count += 1
+		# else:
+			# print(line)
+	print('number of lines:', count_lines)
+	print('number of thumbnails:', tn_count)
 
 print_chirpy()
+
+input_filename = sys.argv[-1]
+#  print (text_filename)
+print("Importing text file: '{0}' ...".format(input_filename))
+input_obj = open(input_filename, "r")
+
+parse_file(input_obj)
+
+
